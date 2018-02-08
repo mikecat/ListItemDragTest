635db011f7f7c7fa817c323fdcf5b5d056bbce20
MikeCAT <mikecat@users.noreply.github.com>
2018-02-08 16:06:38 +0900

    initial commit

---
+++ app/src/main/java/a937363e9_abe6_46e9_b863_e8c5085745bb/listitemdragtest/MainActivity.java
@@ -0,0 +1,13 @@
+package a937363e9_abe6_46e9_b863_e8c5085745bb.listitemdragtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
