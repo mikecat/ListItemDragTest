581c3175da33a3c43749a64f3afc3dfb0a9e4d2b
MikeCAT <mikecat@users.noreply.github.com>
2018-02-08 18:21:12 +0900

    add long click listener (experimental)

--- app/src/main/java/a937363e9_abe6_46e9_b863_e8c5085745bb/listitemdragtest/MyViewHolder.java
+++ app/src/main/java/a937363e9_abe6_46e9_b863_e8c5085745bb/listitemdragtest/MyViewHolder.java
@@ -2,6 +2,7 @@ package a937363e9_abe6_46e9_b863_e8c5085745bb.listitemdragtest;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,6 +13,13 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
     public MyViewHolder(Context context) {
         super(View.inflate(context, R.layout.list_item, null));
         itemText = (TextView)itemView.findViewById(R.id.textView);
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.d("item", "long click " + itemText.getText());
+                return true;
+            }
+        });
     }
 
     public void setItemText(String text) {
